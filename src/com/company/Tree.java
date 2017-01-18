3a8a01b13c99335da725dd5947fc1a2473c748e9
keyvan jamshidi jam <j.keyvan@gmail.com>
2017-01-19 00:59:07 +0330

    first add
    adding first files to repository

---
+++ src/com/company/Tree.java
@@ -0,0 +1,38 @@
+package com.company;
+
+/**
+ * Created by kayvan on 1/11/2017.
+ */
+public class Tree {
+
+    public static void main(String[] args){
+
+        keyvan(3);
+    }
+
+    public interface BinNode<E> {
+
+        public E element();
+
+        public void setElement(E v);
+
+        public BinNode<E> left();
+
+        public BinNode<E> right();
+
+        public boolean isLeaf();
+    }
+
+
+    static void keyvan(int... hello) {
+        for (int i : hello) System.out.println(3);
+    }
+
+
+
+
+}
+
+
+
+
